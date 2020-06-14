ec9d8a6f5d506adc93a216f8bcbf1b0def2d9f55
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-06-14 23:41:40 +0900

    firstをpagenateに変える前

--- app/Http/Controllers/Api/UserLocationController.php
+++ app/Http/Controllers/Api/UserLocationController.php
@@ -10,13 +10,13 @@ use Illuminate\Http\Request;
 class UserLocationController extends Controller
 {
     /**
-     * ユーザーの位置情報を取得
+     * ユーザーの位置情報を取得   GETに該当
      */
     public function index(Request $request)
     {
         // start Middleware で処理がいい
         $token = $request->header('X-API-TOKEN');
-
+        \Log::debug($token);
         // $tokenが空か判定。
         if (empty($token)) {
             // 空だったら処理中止
@@ -25,10 +25,37 @@ class UserLocationController extends Controller
         // end Middleware で処理がいい ↓ヒントの箇所はここから
 
         // start 処理を書く
+        $user = User::where('token', '=', $token)->first();
+        \Log::debug('Userは'.$user);
+        \Log::debug('Useridは'.$user->id);
+        $user_location = UserLocation::where('user_id', '=', $user->id)->first();
+        //user_idが$userのlocationを取ってくる
+        \Log::debug('userlocationは'.$user_location);
+        //::はスコープ定義演算子 場所::中身
+        //->はインスタンスプロパティとインスタントメソッドにアクセスする
+        //=>は連想配列で使うやつ
+        //firstは１つだけデータ取得する
+        // 
+
+        //User_locationの連想配列を作る
+
+        //
+
 
         // hint where を使って、userを絞る
+        //$user_location = UserLocation::paginate(10);
+        $user_location = UserLocation::where('user_id'.'='.$user_location)->paginate(10); // ぺーじねーしょんのしょりをする
+        //where(::前のuserlocation内のカラム.'='.カラムの中の値)のやつだけ取り出してpaginateする
 
-        $user_location = UserLocation::paginate(10); // ぺーじねーしょんのしょりをする
+        \Log::debug($user_location);
+        /**
+        $user_location = UserLocation::create([
+            'user_id' => $user->id,
+            'park_id' => $park_id,
+            'longitude' => $longitude,
+            'latitude' => $latitude,
+        ]);
+        */
 
         // end
 
@@ -39,7 +66,7 @@ class UserLocationController extends Controller
     }
 
     /**
-     * ユーザーの位置情報を記録
+     * ユーザーの位置情報を記録      POSTに該当
      */
     public function create(Request $request)
     {
@@ -65,7 +92,7 @@ class UserLocationController extends Controller
             'longitude' => $longitude,
             'latitude' => $latitude,
         ]);
-
+        \Log::debug('デバッグ４'.$user_location);
         return [
             "data" => $user_location
         ];
